ddb8c12f9178fc048503278b9449496258f704cf
Marcos Javier Riganti <marcos.riganti@gmail.com>
2021-06-10 17:46:26 +0200

    adding testing variables

---
+++ admin/src/firebase_config.js
@@ -0,0 +1,7 @@
+export const REACT_APP_FIREBASE_API_KEY =
+  "AIzaSyC_s-lyCXX_UwZZ0CFUESg7EEYlcVj34is";
+export const REACT_APP_AUTH_DOMAIN = "defensoria-sf.firebaseapp.com";
+export const REACT_APP_PROJECT_ID = "defensoria-sf";
+export const REACT_APP_STORAGE_BUCKET = "defensoria-sf.appspot.com";
+export const REACT_APP_MESSAGING_SENDER_ID = "77357345892";
+export const REACT_APP_APP_ID = "1:77357345892:web:8a6f29dd479ea30ded0371";
