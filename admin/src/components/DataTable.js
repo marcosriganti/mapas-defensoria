892c726f0a6e5d78ba3fcab21cd7edcb7d80b737
Marcos Javier Riganti <marcos.riganti@gmail.com>
2021-10-01 14:36:48 +0200

    small message for no records

--- admin/src/components/DataTable.js
+++ admin/src/components/DataTable.js
@@ -40,55 +40,41 @@ export const Table = ({ table, items, loading, onDelete }) => {
             </tr>
           </thead>
           <tbody className="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
-            {items.map(item => {
-              return (
-                <tr
-                  className="text-gray-700 dark:text-gray-400"
-                  key={`td-${item.uid || item.id}`}
-                >
-                  {table.fields.map(field => {
-                    if (field.onTable) {
-                      return (
-                        <td className="px-4 py-3 text-sm">
-                          {typeof item[field.name] == "string" &&
-                            item[field.name]}
-                          {field.type === "tag" &&
-                            item[field.name] &&
-                            item[field.name].map(el => (
-                              <span className="block-inline bg-purple-400 px-2 py-1 text-xs text-white mx-1">
-                                {el.id}
-                              </span>
-                            ))}
-                          {/* {item[field.name]} */}
-                        </td>
-                      );
-                    }
-                    return null;
-                  })}
+            {items.length > 0 ? (
+              items.map(item => {
+                return (
+                  <tr
+                    className="text-gray-700 dark:text-gray-400"
+                    key={`td-${item.uid || item.id}`}
+                  >
+                    {table.fields.map(field => {
+                      if (field.onTable) {
+                        return (
+                          <td className="px-4 py-3 text-sm">
+                            {typeof item[field.name] == "string" &&
+                              item[field.name]}
+                            {field.type === "tag" &&
+                              item[field.name] &&
+                              item[field.name].map(el => (
+                                <span className="block-inline bg-purple-400 px-2 py-1 text-xs text-white mx-1">
+                                  {el.id}
+                                </span>
+                              ))}
+                            {/* {item[field.name]} */}
+                          </td>
+                        );
+                      }
+                      return null;
+                    })}
 
-                  <td className="px-4 py-3 text-sm text-right">
-                    <div className="flex items-center space-x-4 text-sm">
-                      <Link
-                        className="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                        aria-label="Edit"
-                        to={`/${table.collection}/edit/${item.uid || item.id}`}
-                      >
-                        <svg
-                          className="w-5 h-5"
-                          aria-hidden="true"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                        >
-                          <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
-                        </svg>
-                      </Link>
-                      {onDelete && (
-                        <button
-                          onClick={() => {
-                            onDelete(item.uid || item.id);
-                          }}
+                    <td className="px-4 py-3 text-sm text-right">
+                      <div className="flex items-center space-x-4 text-sm">
+                        <Link
                           className="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                          aria-label="Delete"
+                          aria-label="Edit"
+                          to={`/${table.collection}/edit/${
+                            item.uid || item.id
+                          }`}
                         >
                           <svg
                             className="w-5 h-5"
@@ -96,19 +82,45 @@ export const Table = ({ table, items, loading, onDelete }) => {
                             fill="currentColor"
                             viewBox="0 0 20 20"
                           >
-                            <path
-                              fillRule="evenodd"
-                              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                              clip-rule="evenodd"
-                            ></path>
+                            <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
                           </svg>
-                        </button>
-                      )}
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
+                        </Link>
+                        {onDelete && (
+                          <button
+                            onClick={() => {
+                              onDelete(item.uid || item.id);
+                            }}
+                            className="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                            aria-label="Delete"
+                          >
+                            <svg
+                              className="w-5 h-5"
+                              aria-hidden="true"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                clip-rule="evenodd"
+                              ></path>
+                            </svg>
+                          </button>
+                        )}
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan={table.fields.length}>
+                  <div className=" text-sm p-2 text-center">
+                    No hay documentos para mostrar aqui
+                  </div>
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       )}
@@ -133,7 +145,6 @@ export const DataTable = ({ table, onDelete }) => {
     }
     if (previous && action === "prev") {
       query = query.endAt(last);
-      // setPrevious(last);
     }
     const querySnapshot = await query.get();
     querySnapshot.forEach(doc => {
